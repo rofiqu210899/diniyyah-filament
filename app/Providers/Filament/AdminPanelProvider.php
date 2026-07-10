933c3a77f4f724c94a9710be3734e070cffa1b83
rofiqu2108 <arifrofiqurrohman@gmail.com>
2026-07-10 12:31:06 +0700

    merubah dari MADINA BLOKAGUNG ke MADINA DARUSSALAM

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->brandName('MADINA BLOKAGUNG')
+            ->brandName('MADINA DARUSSALAM')
             ->login(\App\Filament\Pages\Auth\CustomLogin::class)
             ->colors([
                 'primary' => '#198754',
